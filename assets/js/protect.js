5be796699757275bc006421889c0245d8b9a0977
mf-Mii <mf-Mii@outlook.jp>
2021-08-01 15:35:30 +0900

    Add new image protect

---
+++ assets/js/protect.js
@@ -0,0 +1,21 @@
+
+var rclick_cnt = 0;
+$('.img-prot').on("contextmenu", function (e){
+    rclick_cnt++;
+    if(rclick_cnt >= 3){
+        rclick_cnt = 0;
+        $('body div.protect-alert').css("display", "block");
+        $('body div.protect-alert').css("opacity", "1");
+        setTimeout(function (){
+            $('body div.protect-alert').css("opacity", "0");
+        }, 1250);
+        setTimeout(function (){
+            $('body div.protect-alert').css("display", "none");
+        }, 1500);
+    }
+    setTimeout(function (){
+        if (rclick_cnt != 0) rclick_cnt--;
+    }, 10000);
+    return false;
+});
+$('body div.protect-alert, .dis-rclick').on("contextmenu", function (){return false;});
