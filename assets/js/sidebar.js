9caa0642afdf6d18d67154b0ea4663e169cfb796
mf-Mii <67968408+mf-Mii@users.noreply.github.com>
2021-12-31 03:30:07 +0900

    メニューバーとグリッドの関係修正、定形ページの作成

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,29 +1,29 @@
 //pc_menu_open
-$('.site-contents .menu_tab .open .pc').on('click', function(){
+$('.site-contents .menu_tab .open .pc').on('click', function() {
     console.log($(this));
-    if($('.site-contents .menu_tab').hasClass('is_close')){
+    if ($('.site-contents .menu_tab').hasClass('is_close')) {
         //OPEN
         console.log("Opening sidemenu")
         $('.site-contents .menu_tab').animate({
             "left": "0px"
         }, 500);
-        $('.site-contents * .cont').each(function (){
+        $('.site-contents * .cont_box').each(function() {
             let b = $(this);
             b.css("width", "calc(100% - 450px)");
         });
-        setTimeout(function (){
+        setTimeout(function() {
             $('.site-contents .menu_tab').removeClass('is_close');
         }, 500);
-    }else{
+    } else {
         console.log("Closing sidemenu");
         //CLOSE
         $('.site-contents .menu_tab').animate({
             "left": "-400px"
         }, 500);
-        setTimeout(function (){
-            $('.site-contents * .cont').each(function (){
+        setTimeout(function() {
+            $('.site-contents * .cont_box').each(function() {
                 let b = $(this);
-                b.css("width","100%");
+                b.css("width", "100%");
             });
         }, 100);
         $('.site-contents .menu_tab').addClass('is_close');
